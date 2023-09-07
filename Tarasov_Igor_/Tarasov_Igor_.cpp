e2e6fad0e845a9f409c3347dd390d830db1ac7c5
Igor20264 <76581675+Igor20264@users.noreply.github.com>
2023-09-07 23:18:11 +0300

    .

--- Tarasov_Igor_/Tarasov_Igor_.cpp
+++ Tarasov_Igor_/Tarasov_Igor_.cpp
@@ -7,7 +7,7 @@
 #include <fcntl.h> // _O_U16TEXT
 using namespace std;
 
-
+// https://metanit.com/cpp/tutorial/7.4.php
 // https://ru.stackoverflow.com/questions/459154/%D0%A0%D1%83%D1%81%D1%81%D0%BA%D0%B8%D0%B9-%D1%8F%D0%B7%D1%8B%D0%BA-%D0%B2-%D0%BA%D0%BE%D0%BD%D1%81%D0%BE%D0%BB%D0%B8
 // Фик русского языка если не работает setlocale(LC_ALL,"rus")
 void fix() { // позволяет юзать русские символы. Со стак овер флов
